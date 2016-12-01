b9c6927487959f6d14a8471a3b4cd813047f0485
lqm <740735662@qq.com>
2016-12-01 15:59:04 +0800

    Initial Commit

---
+++ LMCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMCollectionView
+//
+//  Created by lqm on 16/12/1.
+//  Copyright © 2016年 LQM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMCollectionView
+//
+//  Created by lqm on 16/12/1.
+//  Copyright © 2016年 LQM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
